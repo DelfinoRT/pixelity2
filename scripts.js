045c0b5cf2adaf215ab3ccd79341cea554e48164
DelfinoRT <delfino.rt@gmail.com>
2024-02-09 16:19:38 +0100

    cleaning css

---
+++ scripts.js
@@ -0,0 +1,15 @@
+ // Script to open and close sidebar
+ function action_open() {
+    if (document.getElementById("mySidebar").style.display === "block") {
+      document.getElementById("mySidebar").style.display = "none";
+      document.getElementById("myOverlay").style.display = "none";
+    } else {
+      document.getElementById("mySidebar").style.display = "block";
+      document.getElementById("myOverlay").style.display = "block";
+    }
+  }
+   
+  function action_close() {
+    document.getElementById("mySidebar").style.display = "none";
+    document.getElementById("myOverlay").style.display = "none";
+  }
