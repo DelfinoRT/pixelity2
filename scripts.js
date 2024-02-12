1bb74e3e17562e5d884683707073346273680c36
DelfinoRT <delfino.rt@gmail.com>
2024-02-12 08:26:53 +0100

    content

--- scripts.js
+++ scripts.js
@@ -14,15 +14,3 @@
     document.getElementById("myOverlay").style.display = "none";
   }
 
-  // Function to get or set the visit count from/to localStorage
-function updateVisitCount() {
-  let count = localStorage.getItem('visitCount');
-  count = count ? parseInt(count) + 1 : 1;
-  localStorage.setItem('visitCount', count);
-  document.getElementById('visitCount').textContent = count;
-}
-
-// Call the function when the page loads
-document.addEventListener('DOMContentLoaded', function() {
-  updateVisitCount();
-});
