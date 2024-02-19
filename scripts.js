516c5fb6307d78f8075e8d023ddbb9fe7e495248
DelfinoRT <46959449+DelfinoRT@users.noreply.github.com>
2024-02-19 20:08:24 +0100

    formatos

--- scripts.js
+++ scripts.js
@@ -1,16 +1,22 @@
- // Script to open and close sidebar
- function action_open() {
-    if (document.getElementById("mySidebar").style.display === "block") {
-      document.getElementById("mySidebar").style.display = "none";
-      document.getElementById("myOverlay").style.display = "none";
-    } else {
-      document.getElementById("mySidebar").style.display = "block";
-      document.getElementById("myOverlay").style.display = "block";
-    }
+// Script to open and close sidebar
+function action_open() {
+  let sidebar = document.getElementById("mySidebar");
+  let overlay = document.getElementById("myOverlay");
+  if (sidebar.style.display === "block") {
+    sidebar.style.display = "none";
+    overlay.style.display = "none";
+  } else {
+    sidebar.style.display = "block";
+    overlay.style.display = "block";
   }
-   
-  function action_close() {
-    document.getElementById("mySidebar").style.display = "none";
-    document.getElementById("myOverlay").style.display = "none";
-  }
+}
 
+function action_close() {
+  document.getElementById("mySidebar").style.display = "none";
+  document.getElementById("myOverlay").style.display = "none";
+}
+
+const doc = new jsPDF();
+
+doc.text("Hello world!", 10, 10);
+doc.save("a4.pdf");
